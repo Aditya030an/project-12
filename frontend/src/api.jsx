849fae4a2f27ad319f90abb1cc9149a557f61c2c
Aditya030an <adityaanu20023@gmail.com>
2025-02-10 05:46:55 +0530

    changing the frontend env file

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/events";
+// const API_URL = "http://localhost:5000/api/events";
+const API_URL = `${import.meta.env.VITE_BASE_URL}/api/events`;
 
 export const fetchEvents = async () => {
   try {
